f4e99c3e69caa8f05675eb0b02b89f6c297973c1
xenarchive <wangxianuvs@gmail.com>
2025-07-18 21:29:55 +0530

    api authentication done

---
+++ api/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cookieParser from 'cookie-parser'
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running!");
+});
